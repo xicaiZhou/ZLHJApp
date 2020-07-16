4046c0ff856f205e1bd841ffef9f166e48dc37e1
xicaiZhou <zhouxicaijob@163.com>
2020-07-16 09:48:55 +0800

    no message

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     state:{
-        token:'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ3ZWIiLCJpc3MiOiJzcHJpbmctYm9vdC1wbHVzIiwiZXhwIjoxNTk0NzI3MjAwLCJpYXQiOjE1OTQ2OTEyMDAsImp0aSI6ImVhZjVhMmQyNTdhZTRlYjI4OGVkYmIxMWI3OTVkNzIyIiwidXNlcm5hbWUiOiJhZG1pbiJ9._MniKISbytlXmRC9Nw3776AuxPzaeu7wAKsaeq6TDmk',
+        token:'',
         loanId:'',
         loanNumber:'20200707153020238',
         // loanNumber:'20200703105318095',
